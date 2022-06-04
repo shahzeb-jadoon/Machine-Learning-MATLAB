5a8dadddf392a6e6f5eabe3fe646a63be3446408
Shahzeb Jadoon <shahzeb.jadoon.sj@gmail.com>
2022-06-03 22:03:43 -0500

    Update code files to run ex3.mlx

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -17,9 +17,12 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-J = ((1 / m) * (((-y)' * log(sigmoid(X * theta))) - ((1 - y)' * log(1 - sigmoid(X * theta))))) + ((lambda / (2 * m)) * sum(theta(2:size(theta, 1)) .^ 2));
+J = ((1 / m) * (((-y)' * log(sigmoid(X * theta))) - ...
+    ((1 - y)' * log(1 - sigmoid(X * theta))))) + ...
+    ((lambda / (2 * m)) * sum(theta(2:size(theta, 1)) .^ 2));
 
-grad = ((1 / m) * X' * (sigmoid(X * theta) - y)) + [0; ((lambda / m) * theta(2:size(theta, 1)))];
+grad = ((1 / m) * X' * (sigmoid(X * theta) - y)) + ...
+        [0; ((lambda / m) * theta(2:size(theta, 1)))];
 
 % =============================================================
 
