ce2e1dae63d37ed71325ab12711b403720e27c3c
Shahzeb Jadoon <shahzeb.jadoon.sj@gmail.com>
2022-06-07 12:26:32 -0500

    Update code files to run ex7.mlx

--- ex7/findClosestCentroids.m
+++ ex7/findClosestCentroids.m
@@ -1,7 +1,7 @@
 function idx = findClosestCentroids(X, centroids)
 %FINDCLOSESTCENTROIDS computes the centroid memberships for every example
 %   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
-%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   in idx for a dataset X where each row is a single example. idx = m x 1
 %   vector of centroid assignments (i.e. each entry in range [1..K])
 %
 
@@ -15,19 +15,43 @@ idx = zeros(size(X,1), 1);
 % Instructions: Go over every example, find its closest centroid, and store
 %               the index inside idx at the appropriate location.
 %               Concretely, idx(i) should contain the index of the centroid
-%               closest to example i. Hence, it should be a value in the 
+%               closest to example i. Hence, it should be a value in the
 %               range 1..K
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
 
+x_index = 1;
 
+for x = X'
+    centroid_index = 2;
 
+    centroidsTranspose = centroids';
+    firstCentroid = centroidsTranspose(:, 1);
+    restCentroids = centroidsTranspose(:, 2:end);
+    firstCentroidDistance = x - firstCentroid;
 
+    J = (norm(firstCentroidDistance))^2;
+    idx(x_index) = 1;
 
+    for centroid = restCentroids
+        currentJ = (norm(x - centroid))^2;
 
+        if currentJ < J
+            J = currentJ;
+            idx(x_index) = centroid_index;
+        end
+
+        centroid_index = centroid_index + 1;
+
+    end
+
+    x_index = x_index + 1;
+
+end
 
 % =============================================================
 
 end
+
 
